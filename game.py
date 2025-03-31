31fb056b645d9cd25cc839116eeabca58596543a
immensesenator4 <Wizkid9955@gmail.com>
2025-03-31 11:41:11 -0700

    opponet chose is good for info

--- game.py
+++ game.py
@@ -31,6 +31,7 @@ if input("hosting ye or ne")=="ye":
             except Exception  as e:
                 print(e)
         opp = findspot(data,s)
+        print("opponet chose : "+data)
         if z== s[opp-1]:
             print("you loose")
         elif z == s[opp]:
@@ -63,6 +64,7 @@ else:
         if data == "leave":
             print("temination forced\nConection Lost\nReason : Conection terminated by Host")
             break
+        print("opponet chose : "+data)
         if z== s[opp-1]:
             print("you loose")
         elif z == s[opp]:
